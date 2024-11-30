257b2e2262a09e7483d9c5a476e4b52f454d4fc3
Guy Brown <guybrown@pm.me>
2024-11-30 14:58:27 +1100

    Added hash generator stub

---
+++ frontend/src/components/security/HashGenerator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function HashGenerator() {
+  return (
+    <div>
+      <Typography variant="h4">Hash Generator</Typography>
+      <Typography variant="body1">
+        This feature is under construction.
+      </Typography>
+    </div>
+  );
+}
+
+export default HashGenerator;
