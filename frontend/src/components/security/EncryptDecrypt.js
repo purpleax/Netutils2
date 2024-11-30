a7a96d6b2dddca2bf12246a8510450c2d90b23cc
Guy Brown <guybrown@pm.me>
2024-11-30 15:02:49 +1100

    updates a few placeholders

---
+++ frontend/src/components/security/EncryptDecrypt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function EncryptDecrypt() {
+  return (
+    <div>
+      <Typography variant="h4">Data Encryption/Decryption Tool</Typography>
+      <Typography variant="body1">
+        This feature is under construction.
+      </Typography>
+    </div>
+  );
+}
+
+export default EncryptDecrypt;
---
+++ frontend/src/components/security/HeaderAnalysis.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function HeaderAnalysis() {
+  return (
+    <div>
+      <Typography variant="h4">HTTP Header Security Analysis</Typography>
+      <Typography variant="body1">
+        This feature is under construction.
+      </Typography>
+    </div>
+  );
+}
+
+export default HeaderAnalysis;
